6a5d866d72f292889ba4fe056fc491346deba658
clyntonjae <clynton.manaois@fumaco.com>
2021-10-02 09:10:37 +0800

    FEC-103
    Dashboard - CMS

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -15,7 +15,7 @@
 			<div class="col-sm-6">
 			  <ol class="breadcrumb float-sm-right">
 				 <li class="breadcrumb-item"><a href="#">Home</a></li>
-				 <li class="breadcrumb-item active">Dashboard v1</li>
+				 <li class="breadcrumb-item active">Dashboard</li>
 			  </ol>
 			</div><!-- /.col -->
 		 </div><!-- /.row -->
@@ -32,14 +32,13 @@
 			  <!-- small box -->
 			  <div class="small-box bg-info">
 				 <div class="inner">
-					<h3>150</h3>
+					<h3>0</h3>
 
 					<p>New Orders</p>
 				 </div>
 				 <div class="icon">
 					<i class="ion ion-bag"></i>
 				 </div>
-				 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
 			  </div>
 			</div>
 			<!-- ./col -->
@@ -47,14 +46,13 @@
 			  <!-- small box -->
 			  <div class="small-box bg-success">
 				 <div class="inner">
-					<h3>53<sup style="font-size: 20px">%</sup></h3>
+					<h3>0</h3>
 
-					<p>Bounce Rate</p>
+					<p>Total Order</p>
 				 </div>
 				 <div class="icon">
 					<i class="ion ion-stats-bars"></i>
 				 </div>
-				 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
 			  </div>
 			</div>
 			<!-- ./col -->
@@ -62,14 +60,13 @@
 			  <!-- small box -->
 			  <div class="small-box bg-warning">
 				 <div class="inner">
-					<h3>44</h3>
+					<h3>0</h3>
 
 					<p>User Registrations</p>
 				 </div>
 				 <div class="icon">
 					<i class="ion ion-person-add"></i>
 				 </div>
-				 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
 			  </div>
 			</div>
 			<!-- ./col -->
@@ -77,14 +74,13 @@
 			  <!-- small box -->
 			  <div class="small-box bg-danger">
 				 <div class="inner">
-					<h3>65</h3>
+					<h3>0</h3>
 
-					<p>Unique Visitors</p>
+					<p>Web Site Visitors</p>
 				 </div>
 				 <div class="icon">
 					<i class="ion ion-pie-graph"></i>
 				 </div>
-				 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
 			  </div>
 			</div>
 			<!-- ./col -->
@@ -93,21 +89,17 @@
 		 <!-- Main row -->
 		 <div class="row">
 			<!-- Left col -->
-			<section class="col-lg-7 connectedSortable">
+			<section class="col-lg-12 connectedSortable">
 			  <!-- Custom tabs (Charts with tabs)-->
 			  <div class="card">
 				 <div class="card-header">
 					<h3 class="card-title">
-					  <i class="fas fa-chart-pie mr-1"></i>
-					  Sales
+					  <i class="fas fa-chart-pie mr-1"></i> Online Sales 
 					</h3>
 					<div class="card-tools">
 					  <ul class="nav nav-pills ml-auto">
 						 <li class="nav-item">
-							<a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
-						 </li>
-						 <li class="nav-item">
-							<a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+							<a class="nav-link active" href="#revenue-chart" data-toggle="tab">Total Sales</a>
 						 </li>
 					  </ul>
 					</div>
