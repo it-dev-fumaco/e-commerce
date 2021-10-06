c1825f4396406b39b1bb4a47675a6bf045761ad3
it-dev-fumaco <85483372+javekulong@users.noreply.github.com>
2021-10-06 14:01:11 +0800

    FEC-51
    Adjust Product Line Description

--- resources/views/frontend/product_list.blade.php
+++ resources/views/frontend/product_list.blade.php
@@ -76,6 +76,28 @@
             animation-name: fadeInUp;
             -webkit-animation-name: fadeInUp;
         }
+
+        .text {
+            position: relative;
+            font-size: 14px;
+            width: 100%;
+        }
+
+        .text-concat {
+            position: relative;
+            display: inline-block;
+            word-wrap: break-word;
+            overflow: hidden;
+            max-height: 4.8em;
+            line-height: 1.2em;
+            text-align:justify;
+        }
+
+        .text.ellipsis::after {
+            position: absolute;
+            right: -12px; 
+            bottom: 4px;
+        }
     </style>
     <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
     <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js'></script>
