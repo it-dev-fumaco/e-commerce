43414ee01786dd864dfc9d1a94e8100ce445e945
it-dev-fumaco <74591603+it-dev-fumaco@users.noreply.github.com>
2021-10-25 08:31:06 +0800

    Time format
    Tracking

--- resources/views/frontend/track_order.blade.php
+++ resources/views/frontend/track_order.blade.php
@@ -102,7 +102,7 @@
 						<tbody>
 						@foreach ($track_order_details as $order)
 						<tr>
-							<td>{{ date('M d, Y - h:m:s', strtotime($order->track_date)) }}</td>
+							<td>{{ date('M d, Y - h:m: A', strtotime($order->track_date)) }}</td>
 							<td><a href="#TrackItemsData" data-toggle="modal">{{ $order->track_item }}</a></td>
 							<td>{{ $order->track_description }}</td>
 							@php
