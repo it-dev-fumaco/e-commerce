765f92bedb13093ad06c3f3ba5ffbcbe91365a1a
clyntonjae <clynton.manaois@fumaco.com>
2021-11-09 08:29:26 +0800

    Update GoogleApiServiceProvider.php

--- app/Providers/GoogleApiServiceProvider.php
+++ app/Providers/GoogleApiServiceProvider.php
@@ -26,14 +26,14 @@ class GoogleApiServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\Schema::hasTable('api_setup')) {
-            $config = Cache::remember('google_api_config', 3600, function () {
-                return DB::table('api_setup')->whereIn('type', ['google_maps_api', 'google_analytics_api'])
-                    ->pluck('api_key', 'type');
-            });
+        // if (\Schema::hasTable('api_setup')) {
+        //     $config = Cache::remember('google_api_config', 3600, function () {
+        //         return DB::table('api_setup')->whereIn('type', ['google_maps_api', 'google_analytics_api'])
+        //             ->pluck('api_key', 'type');
+        //     });
 
-            Config::set('googlemaps.key', $config['google_maps_api']);
-            Config::set('google_api', $config);
-        }
+        //     Config::set('googlemaps.key', $config['google_maps_api']);
+        //     Config::set('google_api', $config);
+        // }
     }
 }
