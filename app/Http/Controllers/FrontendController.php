449b8908ea19a784c2e1c69b5749d0acf6936e98
javekulong <jave.kulong@fumaco.com>
2021-11-09 13:49:42 +0800

    FEC-236
    Address Updating/Editing in Customer Profile Page

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -963,30 +963,31 @@ class FrontendController extends Controller
                 $address_class = $type == 'shipping' ? 'Delivery' : 'Billing';
 
                 $address = DB::table('fumaco_user_add')->where('id', $default->id)->first();
+                if($address){
+                    $address_details = [
+                        'fname' => $address->xcontactname1,
+                        'lname' => $address->xcontactlastname1,
+                        'address_line1' => $address->xadd1,
+                        'address_line2' => $address->xadd2,
+                        'province' => $address->xprov,
+                        'city' => $address->xcity,
+                        'brgy' => $address->xbrgy,
+                        'postal_code' => $address->xpostal,
+                        'country' => $address->xcountry,
+                        'address_type' => $address->add_type,
+                        'business_name' => $address->xbusiness_name,
+                        'tin' => $address->xtin_no,
+                        'email_address' => $address->xcontactemail1,
+                        'mobile_no' => $address->xmobile_number,
+                        'contact_no' => $address->xcontactnumber1,
+                        'same_as_billing' => 0
+                    ];
 
-                $address_details = [
-                    'fname' => $address->xcontactname1,
-                    'lname' => $address->xcontactlastname1,
-                    'address_line1' => $address->xadd1,
-                    'address_line2' => $address->xadd2,
-                    'province' => $address->xprov,
-                    'city' => $address->xcity,
-                    'brgy' => $address->xbrgy,
-                    'postal_code' => $address->xpostal,
-                    'country' => $address->xcountry,
-                    'address_type' => $address->add_type,
-                    'business_name' => $address->xbusiness_name,
-                    'tin' => $address->xtin_no,
-                    'email_address' => $address->xcontactemail1,
-                    'mobile_no' => $address->xmobile_number,
-                    'contact_no' => $address->xcontactnumber1,
-                    'same_as_billing' => 0
-                ];
-
-                if($address_class == 'Delivery'){
-                    session()->put('fumShipDet', $address_details);
-                }else{
-                    session()->put('fumBillDet', $address_details);
+                    if($address_class == 'Delivery'){
+                        session()->put('fumShipDet', $address_details);
+                    }else{
+                        session()->put('fumBillDet', $address_details);
+                    }
                 }
             }
            
@@ -1159,7 +1160,9 @@ class FrontendController extends Controller
                 'address_type' => 'required',
             ]);
 
-            $address_class = ($type == 'billing') ? 'Billing' : 'Delivery';            
+            $address_class = ($type == 'billing') ? 'Billing' : 'Delivery';
+            
+            $checker = DB::table('fumaco_user_add')->where('user_idx', Auth::user()->id)->where('address_class', $address_class)->count();
 
             DB::table('fumaco_user_add')->insert(
                 [
@@ -1180,6 +1183,7 @@ class FrontendController extends Controller
                     'xcontactemail1' => $request->email_address,
                     'xbusiness_name' => ($request->address_type == 'Business Address') ? $request->business_name : null,
                     'xtin_no' => $request->tin_no,
+                    'xdefault' => $checker > 0 ? 0 : 1
                 ]
             );
 
