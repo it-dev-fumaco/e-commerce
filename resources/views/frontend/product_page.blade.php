b33759716f9a50036f3a1fe018541a6ed76b2f9c
clyntonjae <clynton.manaois@fumaco.com>
2021-11-25 16:07:09 +0800

    FEC-43
    Facebook Product Sharing - add og:description

--- resources/views/frontend/product_page.blade.php
+++ resources/views/frontend/product_page.blade.php
@@ -17,6 +17,7 @@
 	<meta property="og:url" content="https://www.fumaco.com/product/{{ ($product_details->slug) ? $product_details->slug : $product_details->id }}" />
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content="{{ $product_details->f_name_name }}" />
+	<meta property="og:description" content="{{ $product_details->f_description }}" />
 	<meta property="og:image" content="{{ asset($src) }}" />
 	<meta property="og:image:width" content="600" >
 	<meta property="og:image:height" content="315" >
