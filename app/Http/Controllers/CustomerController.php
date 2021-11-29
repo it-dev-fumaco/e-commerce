cfa08055b2e651045a1febbc73d520a0d84f06ec
clyntonjae <clynton.manaois@fumaco.com>
2021-11-29 15:54:32 +0800

    customer list sort by date created

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -12,9 +12,10 @@ class CustomerController extends Controller
 {
     public function viewCustomers(Request $request){
         $user_info = DB::table('fumaco_users')->where('is_email_verified', 1)
-        ->when($request->q, function($c) use ($request) {
-            $c->where('f_name', 'LIKE', '%'.$request->q.'%')->orWhere('f_lname', 'LIKE', '%'.$request->q.'%')->orWhere('username', 'LIKE', '%'.$request->q.'%');
-        })
+            ->when($request->q, function($c) use ($request) {
+                $c->where('f_name', 'LIKE', '%'.$request->q.'%')->orWhere('f_lname', 'LIKE', '%'.$request->q.'%')->orWhere('username', 'LIKE', '%'.$request->q.'%');
+            })
+            ->orderBy('created_at', 'desc')
             ->paginate(10);
 
         $user_arr = [];
