72f7552faf918d11e62abe6ea27dd469880631d8
clyntonjae <clynton.manaois@fumaco.com>
2021-12-06 10:04:10 +0800

    display voucher code and shipping name in order history

--- resources/views/frontend/orders.blade.php
+++ resources/views/frontend/orders.blade.php
@@ -119,7 +119,7 @@
 													@endif
 													<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important">
 														<td></td>
-														<td colspan=2 class="table-text" style="text-align: right;">Shipping Fee: </td>
+														<td colspan=2 class="table-text" style="text-align: right;">{{ $order['shipping_name'] }}: </td>
 														<td class="table-text" style="text-align: left; white-space: nowrap !important">₱ {{ number_format($order['shipping_fee'], 2) }}</td>
 													</tr>
 													<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important; font-weight: 700 !important">
@@ -150,7 +150,7 @@
 													@endif
 													<div class="row m-1">
 														<div class="col-md-10" style="text-align: right;">
-															<span>Shipping Fee: </span>
+															<span>{{ $order['shipping_name'] }}: </span>
 														</div>
 														<div class="col-md-2" style="text-align: right;">
 															<span>₱ {{ number_format($order['shipping_fee'], 2) }}</span>
@@ -228,142 +228,6 @@
 									</tr>
 								@endforelse
 							</div>
-							{{-- <table class="table">
-								<thead>
-									<tr>
-										<th class="text-center d-none d-sm-table-cell">Order No.</th>
-										<th class="text-center table-text">Date</th>
-										<th class="text-center table-text">Details</th>
-										<th class="text-center d-none d-sm-table-cell">Shipping</th>
-										<th class="text-center d-none d-sm-table-cell">Estimated Delivery Date</th>
-										<th class="text-center d-none d-sm-table-cell">Status</th>
-									</tr>
-								</thead>
-								<tbody>
-									@forelse ($new_orders_arr as $order)
-									@php
-										if($order['status'] == "Order Placed"){
-											$badge = '#ffc107';
-										}else if($order['status'] == "Delivered"){
-											$badge = '#fd6300';
-										}else if($order['status'] == "Out for Delivery"){
-											$badge = '#28a745';
-										}else{
-											$badge = '#007bff';
-										}
-									@endphp
-									<tr>
-										<td class="text-center align-middle d-none d-sm-table-cell">{{ $order['order_number'] }}</td>
-										<td class="text-center align-middle"><span class="table-text">{{ $order['date'] }}</span></td>
-										<td class="text-center align-middle">
-											<a href="#" class="table-text" data-toggle="modal" data-target="#{{ $order['order_number'] }}-Modal">Item Purchase</a>
-
-											<!-- Modal -->
-											<div class="modal fade" id="{{ $order['order_number'] }}-Modal" tabindex="-1" role="dialog" aria-labelledby="{{ $order['order_number'] }}-ModalLabel" aria-hidden="true">
-												<div class="modal-dialog modal-xl" style="min-width: 70%;">
-													<div class="modal-content">
-														<div class="modal-header">
-															<h5 class="modal-title">{{ $order['order_number'] }}</h5>
-															<button type="button" class="close clear-bg" data-dismiss="modal" aria-label="Close">
-																<span aria-hidden="true">&times;</span>
-															</button>
-														</div>
-														<div class="modal-body">
-															<table class="table" style="width: 100% !important;">
-																<tr>
-																	<th class="col-sm-1"></th>
-																	<th class="col-sm-3">Item Name</th>
-																	<th class="col-sm-2">Quantity</th>
-																	<th class="col-sm-2">Price</th>
-																</tr>
-																@foreach ($order['items'] as $item)
-																	<tr>
-																		<td>
-																			<img src="{{ asset('/storage/item_images/'.$item['item_code'].'/gallery/preview/'.$item['image']) }}" class="img-responsive" alt="" width="55" height="55">
-																		</td>
-																		<td>{{ $item['item_name'] }}</td>
-																		<td>{{ $item['qty'] }}</td>
-																		@php
-																			$orig_price = number_format($item['orig_price'], 2);
-																			$price = number_format($item['price'], 2);
-																		@endphp
-																		<td>
-																			@if($item['discount'] > 0)
-																				<p><span style="text-decoration: line-through; font-size: 9pt;">₱ {{ $orig_price }}</span><br/><b>₱ {{ $price }}</b></p>
-																			@else
-																				<p>₱ {{ $price }}</p>
-																			@endif
-																		</td>
-																	</tr>
-																@endforeach
-																<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important">
-																	<td></td>
-																	<td colspan=2 style="text-align: right;">Subtotal: </td>
-																	<td style="text-align: left; white-space: nowrap !important">₱ {{ number_format($order['subtotal'], 2) }}</td>
-																</tr>
-																<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important">
-																	<td></td>
-																	<td colspan=2 style="text-align: right;">Shipping Fee: </td>
-																	<td style="text-align: left; white-space: nowrap !important">₱ {{ number_format($order['shipping_fee'], 2) }}</td>
-																</tr>
-																<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important; font-weight: 700 !important">
-																	<td></td>
-																	<td colspan=2 style="text-align: right;">Grand Total: </td>
-																	<td style="text-align: left; white-space: nowrap !important">₱ {{ number_format($order['grand_total'], 2) }}</td>
-																</tr>
-															</table>
-															<div class="d-none d-xl-block">
-																<div class="row">
-																	<div class="col-md-10" style="text-align: right;">
-																		<span>Subtotal: </span>
-																	</div>
-																	<div class="col-md-2" style="text-align: left;">
-																		<span>₱ {{ number_format($order['subtotal'], 2) }}</span>
-																	</div><br/>&nbsp;
-																</div>
-																<div class="row">
-																	<div class="col-md-10" style="text-align: right;">
-																		<span>Shipping Fee: </span>
-																	</div>
-																	<div class="col-md-2" style="text-align: left;">
-																		<span>₱ {{ number_format($order['shipping_fee'], 2) }}</span>
-																	</div><br/>&nbsp;
-																</div>
-																<div class="row">
-																	<div class="col-md-10" style="text-align: right;">
-																		<span style="font-weight: 700">Grand Total: </span>
-																	</div>
-																	<div class="col-md-2" style="text-align: left;">
-																		<span style="font-weight: 700">₱ {{ number_format($order['grand_total'], 2) }}</span>
-																	</div><br/>&nbsp;
-																</div>
-															</div>
-														</div>
-														<div class="modal-footer">
-															<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="d-md-none table-text" style="text-align: left;">
-												<br/>
-												<b>Order Number:</b><br/>{{ $order['order_number'] }}<br/><br/>
-												<b>Shipping Name:</b> {{ $order['shipping_name'] }}<br/><br/>
-												<b>Est. Delivery Date:</b><br class="d-sm-block d-md-none"/> {{ $order['edd'] }}<br/>
-												<span class="badge text-dark" style="background-color: {{ $badge }}; font-size: 0.9rem; color: #fff !important;">{{ $order['status'] }}</span>
-											</div>
-										</td>
-										<td class="text-center align-middle d-none d-sm-table-cell">{{ $order['shipping_name'] }}</td>
-										<td class="text-center align-middle d-none d-sm-table-cell">{{ $order['edd'] }}</td>
-										<td class="text-center align-middle d-none d-sm-table-cell"><span class="badge text-dark" style="background-color: {{ $badge }}; font-size: 0.9rem; color: #fff !important;">{{ $order['status'] }}</span></td>
-									</tr>
-									@empty
-										<tr>
-											<td class="text-center p-3" colspan="6">No transactions found.</td>
-										</tr>
-									@endforelse
-								</tbody>
-							</table> --}}
 						</div>
 						<div id="history" class="container tab-pane fade">
 							<br>
@@ -425,7 +289,7 @@
 																		<td>
 																			<img src="{{ asset('/storage/item_images/'.$item['item_code'].'/gallery/preview/'.$item['image']) }}" class="img-responsive" alt="" width="55" height="55">
 																		</td>
-																		<td>{{ $item['item_name'] }}</td>
+																		<td style="text-align: left;">{{ $item['item_name'] }}</td>
 																		<td>{{ $item['qty'] }}</td>
 																		@php
 																			$orig_price = number_format($item['orig_price'], 2);
@@ -443,41 +307,60 @@
 																<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important">
 																	<td></td>
 																	<td colspan=2 style="text-align: right;">Subtotal: </td>
-																	<td style="text-align: left; white-space: nowrap !important">₱ {{ number_format($order['subtotal'], 2) }}</td>
+																	<td style="text-align: right; white-space: nowrap !important">₱ {{ number_format($order['subtotal'], 2) }}</td>
 																</tr>
+																@if ($order['voucher_code'])
 																<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important">
 																	<td></td>
-																	<td colspan=2 style="text-align: right;">Shipping Fee: </td>
-																	<td style="text-align: left; white-space: nowrap !important">₱ {{ number_format($order['shipping_fee'], 2) }}</td>
+																	<td colspan=2 class="table-text" style="text-align: right;">Discount: <span class="text-white" style="border: 1px dotted #ffff; padding: 3px 8px; margin: 2px; font-size: 7pt; background-color:#1c2833;">{{ $order['voucher_code'] }}</span>
+																		</td>
+																	<td class="table-text" style="text-align: right; white-space: nowrap !important">₱ {{ number_format($order['discount_amount'], 2) }}</td>
+																</tr>
+																@endif
+																<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important">
+																	<td></td>
+																	<td colspan=2 style="text-align: right;">{{ $order['shipping_name'] }}: </td>
+																	<td style="text-align: right; white-space: nowrap !important">₱ {{ number_format($order['shipping_fee'], 2) }}</td>
 																</tr>
 																<tr class="d-lg-none d-xl-none" style="border-bottom: rgba(0,0,0,0) !important; font-weight: 700 !important">
 																	<td></td>
 																	<td colspan=2 style="text-align: right;">Grand Total: </td>
-																	<td style="text-align: left; white-space: nowrap !important">₱ {{ number_format($order['grand_total'], 2) }}</td>
+																	<td style="text-align: right; white-space: nowrap !important">₱ {{ number_format($order['grand_total'], 2) }}</td>
 																</tr>
 															</table>
 															<div class="d-none d-xl-block">
-																<div class="row">
+																<div class="row m-1">
 																	<div class="col-md-10" style="text-align: right;">
 																		<span>Subtotal: </span>
 																	</div>
-																	<div class="col-md-2" style="text-align: left;">
+																	<div class="col-md-2" style="text-align: right;">
 																		<span>₱ {{ number_format($order['subtotal'], 2) }}</span>
-																	</div><br/>&nbsp;
-																</div>
-																<div class="row">
-																	<div class="col-md-10" style="text-align: right;">
-																		<span>Shipping Fee: </span>
 																	</div>
-																	<div class="col-md-2" style="text-align: left;">
-																		<span>₱ {{ number_format($order['shipping_fee'], 2) }}</span>
-																	</div><br/>&nbsp;
 																</div>
-																<div class="row">
+																@if ($order['voucher_code'])
+																<div class="row m-1">
+																	<div class="col-md-10" style="text-align: right;">
+																		<span>Discount:  <span class="text-white" style="border: 1px dotted #ffff; padding: 3px 8px; margin: 2px; font-size: 7pt; background-color:#1c2833;">{{ $order['voucher_code'] }}</span>
+																		</span>
+																	</div>
+																	<div class="col-md-2" style="text-align: right;">
+																		<span>₱ {{ number_format($order['discount_amount'], 2) }}</span>
+																	</div>
+																</div>
+																@endif
+																<div class="row m-1">
+																	<div class="col-md-10" style="text-align: right;">
+																		<span>{{ $order['shipping_name'] }}: </span>
+																	</div>
+																	<div class="col-md-2" style="text-align: right;">
+																		<span>₱ {{ number_format($order['shipping_fee'], 2) }}</span>
+																	</div>
+																</div>
+																<div class="row m-1">
 																	<div class="col-md-10" style="text-align: right;">
 																		<span style="font-weight: 700">Grand Total: </span>
 																	</div>
-																	<div class="col-md-2" style="text-align: left;">
+																	<div class="col-md-2" style="text-align: right;">
 																		<span style="font-weight: 700">₱ {{ number_format($order['grand_total'], 2) }}</span>
 																	</div><br/>&nbsp;
 																</div>
