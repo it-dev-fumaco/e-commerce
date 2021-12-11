da66fb68bdf1d57fea9ec06a25dd4da86dce0ef8
it-dev-fumaco <74591603+it-dev-fumaco@users.noreply.github.com>
2021-12-11 13:33:54 +0800

    Integrate Google Tag Manager

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -39,14 +39,14 @@
 
     gtag('config', '{{ Config::get('google_api.google_analytics_api') }}');
     </script>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NZJWSRR');</script>
-    <!-- End Google Tag Manager -->
     @endif
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NZJWSRR');</script>
+            <!-- End Google Tag Manager -->
 
             <!-- Messenger Chat Plugin Code -->
 <div id="fb-root"></div>
