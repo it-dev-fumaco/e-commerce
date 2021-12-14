557df035c9ff64c6dc3dea3937fbe0eb65e4124f
javekulong <jave.kulong@fumaco.com>
2021-12-14 10:17:52 +0800

    Revert "FEC-229"
    This reverts commit 46a8b85ea4dee41437be8c0b1bd3be49fcdc1f06.

--- resources/views/backend/search_terms.blade.php
+++ resources/views/backend/search_terms.blade.php
@@ -67,7 +67,7 @@
                                                         <div class="modal-dialog modal-xl" role="document">
                                                             <div class="modal-content">
                                                             <div class="modal-header">
-                                                                <h5 class="modal-title" id="exampleModalLabel">Search Term: {{ $terms['search_term'] }}</h5>
+                                                                <h5 class="modal-title" id="exampleModalLabel">Search Results</h5>
                                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                                 <span aria-hidden="true">&times;</span>
                                                                 </button>
