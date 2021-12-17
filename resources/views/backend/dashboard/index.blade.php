5bf67173e7a5632bd9d4504b58f8660f0f04d10c
javekulong <jave.kulong@fumaco.com>
2021-12-17 17:00:28 +0800

    FEC-308
    Display monthly sales report chart with filter per year

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -88,7 +88,7 @@
 		 <!-- /.row -->
 		 <!-- Main row -->
 		 <div class="row">
-			{{-- <!-- Left col -->
+			<!-- Left col -->
 			<section class="col-6 connectedSortable">
 			  <!-- Custom tabs (Charts with tabs)-->
 			  <div class="card">
@@ -97,29 +97,45 @@
 					  <i class="fas fa-chart-pie mr-1"></i> Online Sales 
 					</h3>
 					<div class="card-tools">
-					  <ul class="nav nav-pills ml-auto">
-						 <li class="nav-item">
-							<a class="nav-link active" href="#revenue-chart" data-toggle="tab">Total Sales</a>
-						 </li>
-					  </ul>
+						<ul class="nav nav-pills ml-auto">
+							<li class="nav-item">
+								{{-- <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Total Sales</a> --}}
+								<form action="/admin/dashboard" method="get">
+									<div class="btn-group col-12" role="group">
+										<select name="year" class="form-control" required> 
+											<option value="" disabled>Year</option>
+											@foreach ($sales_year as $year)
+												@php
+													$selected = null;
+													if(!request()->get('year')){
+														if(\Carbon\Carbon::now()->format('Y') == $year->{'YEAR(order_date)'}){
+															$selected = 'selected';
+														}
+													}else{
+														if($year->{'YEAR(order_date)'} == request()->get('year')){
+															$selected = 'selected';
+														}
+													}
+												@endphp
+												<option {{ $selected }} value="{{ $year->{'YEAR(order_date)'} }}">{{ $year->{'YEAR(order_date)'} }}</option>
+											@endforeach
+										</select>
+										<button type="submit" class="btn btn-primary btn-sm pl-3 pr-2"><i class="fas fa-search mr-1"></i></button>
+									</div>
+								</form>
+							</li>
+						</ul>
 					</div>
 				 </div><!-- /.card-header -->
 				 <div class="card-body">
 					<div class="tab-content p-0">
-					  <!-- Morris chart - Sales -->
-					  <div class="chart tab-pane active" id="revenue-chart"
-							 style="position: relative; height: 300px;">
-							<canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
-						</div>
-					  <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
-						 <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
-					  </div>
+					  	<canvas id="myChart" style="width:100%;"></canvas>
 					</div>
 				 </div><!-- /.card-body -->
 			  </div>
 			  <!-- /.card -->
 			</section>
-			<!-- /.Left col --> --}}
+			<!-- /.Left col -->
 
 			<div class="col-6">
 				<div class="card">
@@ -151,4 +167,38 @@
 	</section>
 	<!-- /.content -->
  </div>
+@endsection
+@section('script')
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+
+<script>
+	var xValues = ["January", "February", 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	var yValues = [{{ collect($sales_arr)->pluck('sales')->implode(',') }}];
+	new Chart("myChart", {
+		type: "line",
+		data: {
+			labels: xValues,
+			datasets: [{
+			backgroundColor: "rgba(0,123,255,0)",
+			borderColor: "rgba(0,123,255,1)",
+			data: yValues
+			}]
+		},
+		options: {
+			legend: {display: false},
+			title: {
+				display: true,
+				text: "Monthly Sales Report"
+			},
+			tooltips: {
+				callbacks: {
+					label: function(tooltipItem) {
+						return "₱ " + Number(tooltipItem.yLabel).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+					}
+				}
+			}
+		}
+	});
+</script>
 @endsection
