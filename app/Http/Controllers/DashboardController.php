7023e35b46f6ee978d53de7a2c414187c38b23dc
javekulong <jave.kulong@fumaco.com>
2021-12-17 15:36:51 +0800

    FEC-308
    Display monthly sales report chart with filter per year

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ use DB;
 
 class DashboardController extends Controller
 {
-	public function index() {
+	public function index(Request $request) {
 		$users = DB::table('fumaco_users')->where('is_email_verified', 1)->count();
 
 		$orders = DB::table('fumaco_order')->get();
@@ -19,7 +19,22 @@ class DashboardController extends Controller
 
 		$most_searched = DB::table('fumaco_search_terms')->orderBy('frequency', 'desc')->limit(10)->get();
 
-		return view('backend.dashboard.index', compact('new_orders', 'total_orders', 'users', 'total_sales', 'most_searched'));
+		// sales per month
+		$sales_arr = [];
+		for($month = 1; $month <= 12; $month++){
+			if($request->year){
+				$sales = DB::table('fumaco_order')->where('order_status', '!=', 'Cancelled')->whereMonth('order_date', $month)->whereYear('order_date', $request->year)->sum('amount_paid');
+			}else{
+				$sales = DB::table('fumaco_order')->where('order_status', '!=', 'Cancelled')->whereMonth('order_date', $month)->whereYear('order_date', Carbon::now()->format('Y'))->sum('amount_paid');
+			}
+			$sales_arr[] = [
+				'month' => $month,
+				'sales' => number_format((float)$sales, 2, '.', '')
+			];
+		}
+		$sales_year = DB::table('fumaco_order')->selectRaw('YEAR(order_date)')->distinct()->get();
+
+		return view('backend.dashboard.index', compact('new_orders', 'total_orders', 'users', 'total_sales', 'most_searched', 'sales_arr', 'sales_year'));
 	}
 
 }
