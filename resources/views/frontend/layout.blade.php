767b86420e11ee4a82ce629d22fa3cb7002fe2a8
javekulong <85483372+javekulong@users.noreply.github.com>
2022-09-22 14:41:30 +0800

    FEC-568
    Does not use passive listeners to improve scrolling performance

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -697,6 +697,28 @@
       $("#closeCookieConsent, .cookieConsentOK").click(function() {
           $("#cookieConsent").fadeOut(200);
       });
+
+      // Fix for "Does not use passive listeners to improve scrolling performance"
+      jQuery.event.special.touchstart = {
+          setup: function( _, ns, handle ) {
+              this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+          }
+      };
+      jQuery.event.special.touchmove = {
+          setup: function( _, ns, handle ) {
+              this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+          }
+      };
+      jQuery.event.special.wheel = {
+          setup: function( _, ns, handle ){
+              this.addEventListener("wheel", handle, { passive: true });
+          }
+      };
+      jQuery.event.special.mousewheel = {
+          setup: function( _, ns, handle ){
+              this.addEventListener("mousewheel", handle, { passive: true });
+          }
+      };
     });
   </script>
 
