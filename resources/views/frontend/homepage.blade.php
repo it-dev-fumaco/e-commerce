03a57c5db563b8d02026bfc679cba65a3ad9a662
javekulong <85483372+javekulong@users.noreply.github.com>
2022-10-27 08:40:05 +0800

    Testing
    will be reverted

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -89,7 +89,7 @@
       <section class="py-5 text-center container">
         <div class="row py-lg-5">
           <div class="col-lg-6 col-md-8 mx-auto">
-            <h4 class="fw-light font-b2 fumacoFont1">LATEST ARTICLES</h4>
+            <h4 class="fw-light font-b2 fumacoFont1">LATEST ARTICLES 213</h4>
           </div>
         </div>
       </section>
