5c0a7bf9614da0bc598f103971f2b4467752be8f
javekulong <85483372+javekulong@users.noreply.github.com>
2022-12-21 08:25:17 +0800

    FEC-621
    Form to upload banner per category in admin page with banner image preview (to replace default banner)

--- resources/views/backend/category/category.blade.php
+++ resources/views/backend/category/category.blade.php
@@ -91,7 +91,7 @@
 													<div class="modal-dialog modal-xl">
 														<div class="modal-content">
 															<div class="modal-header">
-																<h4 class="modal-title">Edit Category : {{ $c->id }}</h4>
+																<h4 class="modal-title">Edit Category : {{ $c->name }}</h4>
 																<button type="button" class="close" data-dismiss="modal">&times;</button>
 															</div>
 															<div class="modal-body">
@@ -101,10 +101,31 @@
 																				@csrf
 																				<div class="card-body">
 																					<div class="row">
-																						<div class="col-12">
+																						<div class="col-8 offset-2 mb-2">
 																							@if ($c->banner_img)
-																								<img src="{{ asset('storage/banner_images/'.$c->banner_img) }}" width="100%"><br>	
+																								<a href="#" data-toggle="modal" data-target="#preview-banner-{{ $c->id }}">
+																									<img src="{{ asset('storage/banner_images/'.$c->banner_img) }}" width="100%">
+																								</a>
+
+																								<!-- Modal -->
+																								<div class="modal fade" id="preview-banner-{{ $c->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+																									<div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+																										<div class="modal-content">
+																											<div class="modal-header">
+																												{{-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> --}}
+																												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																													<span aria-hidden="true">&times;</span>
+																												</button>
+																											</div>
+																											<div class="modal-body">
+																												<img src="{{ asset('storage/banner_images/'.$c->banner_img) }}" width="100%">
+																											</div>
+																										</div>
+																									</div>
+																								</div>
 																							@endif
+																						</div>
+																						<div class="col-6">
 																							<div class="form-group">
 																								<label for="add_cat_name">Banner Image : </label>
 																								<div class="custom-file">
@@ -113,11 +134,14 @@
 																								</div>
 																							</div>
 																						</div>
+																						{{-- <div class="col-6">&nbsp;</div> --}}
 																						<div class="col-md-6">
 																							<div class="form-group">
 																								<label for="edit_cat_name">Category Name :</label>
 																								<input type="text" class="form-control" id="edit_cat_name" name="edit_cat_name" value="{{ $c->name }}" required>
 																							</div>
+																						</div>
+																						<div class="col-6">
 																							<div class="form-group">
 																								<label for="x2">Image : </label>
 																								<br>
@@ -277,6 +301,9 @@
 	</div>
 </div> 
 <style>
+	.modal{
+		background-color: rgba(0,0,0,0.4);
+	}
 	.switch {
 		position: relative;
 		display: inline-block;
