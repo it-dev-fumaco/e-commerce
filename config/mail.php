1452a061cf14ff2be4f7cdf3bde3f84756b16073
clyntonjae <75285893+clyntonjae@users.noreply.github.com>
2023-04-13 08:58:35 +0800

    FEC-665
    Password Reset is not working

--- config/mail.php
+++ config/mail.php
@@ -43,7 +43,13 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
-            'verify_peer' => env('MAIL_VERIFY_PEER', true),
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ]
         ],
 
         'ses' => [
