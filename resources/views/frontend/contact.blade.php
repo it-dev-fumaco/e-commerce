7036336daf533f893ebdc4bc718dc88df5fc3779
javekulong <85483372+javekulong@users.noreply.github.com>
2023-09-21 11:41:03 +0800

    Revert "FEC-696"
    This reverts commit ca1683a8563503a81eb71e81c607f9e4a40c8a32.

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -164,13 +164,3 @@
     }
   </style>
 @endsection
-
-<script>
-  function captchaSuccess(response){
-    $('#submitBtn').removeClass('disabled-btn').prop('disabled', false)
-  }
-
-  function captchaFail(response){
-    $('#submitBtn').addClass('disabled-btn').prop('disabled', true)
-  }
-</script>
