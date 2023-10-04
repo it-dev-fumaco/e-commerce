b30ac474b0044650c8fb14b42e33457d2c5c4d8d
javekulong <85483372+javekulong@users.noreply.github.com>
2023-10-04 10:49:14 +0800

    FEC-702

--- resources/views/frontend/testing_sso.blade.php
+++ resources/views/frontend/testing_sso.blade.php
@@ -7,17 +7,15 @@
 <body>
     <script>
         window.fbAsyncInit = function() {
-          FB.init({
-            appId      : '596451285825362',
-            cookie     : true,
-            xfbml      : true,
-            version    : 'v18.0'
-          });
-            
-          FB.AppEvents.logPageView();   
-            
+            FB.init({
+                appId      : '596451285825362',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v18.0'
+            });
+                
+            FB.AppEvents.logPageView();  
         };
-      
         (function(d, s, id){
            var js, fjs = d.getElementsByTagName(s)[0];
            if (d.getElementById(id)) {return;}
@@ -25,30 +23,28 @@
            js.src = "https://connect.facebook.net/en_US/sdk.js";
            fjs.parentNode.insertBefore(js, fjs);
          }(document, 'script', 'facebook-jssdk'));
-      </script>
-
-      <script>
-
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-    console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-      testAPI();  
-    } else {                                 // Not logged into your webpage or we are unable to tell.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this webpage.';
-    }
-  }
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
         
+
+         function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+            console.log('statusChangeCallback');
+            console.log(response);                   // The current login status of the person.
+            if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+            testAPI();  
+            } else {                                 // Not logged into your webpage or we are unable to tell.
+            document.getElementById('status').innerHTML = 'Please log ' +
+                'into this webpage.';
+            }
+        }
+
+        function checkLoginState() {
         FB.getLoginStatus(function(response) {
             statusChangeCallback(response);
         });
+        }
+        
+        // FB.getLoginStatus(function(response) {
+        //     statusChangeCallback(response);
+        // });
       </script>
 
 
