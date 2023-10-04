a6fd8e2bbebc3afcf307c014a3021a4bcae988e8
javekulong <85483372+javekulong@users.noreply.github.com>
2023-10-04 10:41:05 +0800

    FEC-702

--- resources/views/frontend/testing_sso.blade.php
+++ resources/views/frontend/testing_sso.blade.php
@@ -5,7 +5,8 @@
 <meta charset="UTF-8">
 </head>
 <body>
-    <script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+  <script>
         window.fbAsyncInit = function() {
           FB.init({
             appId      : '596451285825362',
