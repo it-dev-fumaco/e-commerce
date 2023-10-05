c9fffbeb2ebfb050b519749333d4920173b582f7
javekulong <85483372+javekulong@users.noreply.github.com>
2023-10-05 08:30:03 +0800

    FEC-702

--- resources/views/frontend/testing_sso.blade.php
+++ resources/views/frontend/testing_sso.blade.php
@@ -5,49 +5,6 @@
 <meta charset="UTF-8">
 </head>
 <body>
-    <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId      : '596451285825362',
-                cookie     : true,
-                xfbml      : true,
-                version    : 'v18.0'
-            });
-                
-            FB.AppEvents.logPageView();  
-        };
-        (function(d, s, id){
-           var js, fjs = d.getElementsByTagName(s)[0];
-           if (d.getElementById(id)) {return;}
-           js = d.createElement(s); js.id = id;
-           js.src = "https://connect.facebook.net/en_US/sdk.js";
-           fjs.parentNode.insertBefore(js, fjs);
-         }(document, 'script', 'facebook-jssdk'));
-        
-
-         function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-            console.log('statusChangeCallback');
-            console.log(response);                   // The current login status of the person.
-            if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-            testAPI();  
-            } else {                                 // Not logged into your webpage or we are unable to tell.
-            document.getElementById('status').innerHTML = 'Please log ' +
-                'into this webpage.';
-            }
-        }
-
-        function checkLoginState() {
-        FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
-        });
-        }
-        
-        // FB.getLoginStatus(function(response) {
-        //     statusChangeCallback(response);
-        // });
-      </script>
-
-
 <!-- The JS SDK Login Button -->
 
 
@@ -61,5 +18,46 @@
 
 <!-- Load the JS SDK asynchronously -->
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+<script>
+    // window.fbAsyncInit = function() {
+    //     FB.init({
+    //         appId      : '596451285825362',
+    //         cookie     : true,
+    //         xfbml      : true,
+    //         version    : 'v18.0'
+    //     });
+            
+    //     FB.AppEvents.logPageView();  
+    // };
+    // (function(d, s, id){
+    //    var js, fjs = d.getElementsByTagName(s)[0];
+    //    if (d.getElementById(id)) {return;}
+    //    js = d.createElement(s); js.id = id;
+    //    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    //    fjs.parentNode.insertBefore(js, fjs);
+    //  }(document, 'script', 'facebook-jssdk'));
+    
+
+     function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+        console.log('statusChangeCallback');
+        console.log(response);                   // The current login status of the person.
+        if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+        testAPI();  
+        } else {                                 // Not logged into your webpage or we are unable to tell.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this webpage.';
+        }
+    }
+
+    function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+    }
+    
+    // FB.getLoginStatus(function(response) {
+    //     statusChangeCallback(response);
+    // });
+</script>
 </body>
 </html>
