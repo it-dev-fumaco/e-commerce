ffd155407db33079ca71ead6d97296275e0a71fa
javekulong <85483372+javekulong@users.noreply.github.com>
2023-10-05 09:56:59 +0800

    FEC-702

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -918,17 +918,17 @@
         FB.getLoginStatus(function(response) {
             // statusChangeCallback(response);
             console.log(response)
-            // FB.api('/me', 'GET', { fields: 'email,first_name,last_name' }, function(profile) {
-            //   // Access email, first name, and last name from the profile object
-            //   var email = profile.email;
-            //   var firstName = profile.first_name;
-            //   var lastName = profile.last_name;
+            FB.api('/me', 'GET', { fields: 'email,first_name,last_name' }, function(profile) {
+              // Access email, first name, and last name from the profile object
+              var email = profile.email;
+              var firstName = profile.first_name;
+              var lastName = profile.last_name;
 
-            //   // Now you can use the email, first name, and last name as needed
-            //   console.log('User email: ' + email);
-            //   console.log('User first name: ' + firstName);
-            //   console.log('User last name: ' + lastName);
-            // });
+              // Now you can use the email, first name, and last name as needed
+              console.log('User email: ' + email);
+              console.log('User first name: ' + firstName);
+              console.log('User last name: ' + lastName);
+            });
         });
       }
     </script>
